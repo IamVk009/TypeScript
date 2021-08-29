ef1682c737716ad7ad88266c693f28538fc0587e
IamVk009 <imvk009@gmail.com>
2021-08-29 20:53:45 +0530

    Object Added

--- 1] Data Types/1] Data Types/Object.js
+++ 1] Data Types/1] Data Types/Object.js
@@ -1,11 +1,9 @@
-// Creation of typed object which is similer to creating typed variable.
-// Creating type of an object
 var emp;
-// Creating object of above created type.
+
 emp = {
     id: 100,
     name: "Flash"
 };
-// To access the object properties.
+
 console.log(emp.id);
 console.log(emp.name);
