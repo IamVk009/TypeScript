3bd676cdb0c2be16dc60dac879cb93a6c7c5f86a
IamVk009 <imvk009@gmail.com>
2021-08-30 14:29:03 +0530

    Data Types Added

--- 1] Data Types/2] Array/Arrays.js
+++ 1] Data Types/2] Array/Arrays.js
@@ -1,28 +1,26 @@
-// Arrays are typed in TypeScript.
-// Here we have defined number array so only numbers can be assigned to this array.
+
 var aray;
-// let aray:Array<String>
+
 aray = [10, 20, 30, 40];
 console.log(aray);
 console.log("----------------------------------------------------------------------------------------------");
-// Accessing individual element from array.
-// For loop - 1
+
 for (var x in aray) {
     //console.log(x);
     console.log(aray[x]);
 }
 console.log("----------------------------------------------------------------------------------------------");
-//String Array
+
 var aray1;
 aray1 = ["abc", "cde", "hra"];
 console.log(aray1);
 console.log("----------------------------------------------------------------------------------------------");
-//For loop - 2
+
 for (var i = 0; i < aray1.length; i++) {
     console.log(aray1[i]);
 }
 console.log("----------------------------------------------------------------------------------------------");
-// Tuple allows to define element number and element types for an array.
+
 var tup1;
 tup1 = [10, 20, "Flash", 30];
 console.log(tup1);
