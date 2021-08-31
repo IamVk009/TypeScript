77417be38a74eaf62ff44e91be9a09cbc38847e9
IamVk009 <imvk009@gmail.com>
2021-08-31 19:24:21 +0530

    Function Added

--- 3] Function/Optional.js
+++ 3] Function/Optional.js
@@ -1,14 +1,11 @@
-// Function Declaration in TS.
+
 function f1(fname, lname) {
     console.log(fname);
     console.log(lname);
 }
 f1("Bhuvaneshwar", "Kumar");
 console.log("----------------------------------------------------------------------------------------------");
-// Optional parameter in function in TypeScript.
-// Just add question mark (?) after the parameter to make it optional.
-// Optional parameter should be the last parameter of a function.
-// Here lname is a optional parameter.
+
 function f2(fname, lname) {
     console.log(fname);
     console.log(lname);
@@ -16,7 +13,7 @@ function f2(fname, lname) {
 f2("Bhuvaneshwar");
 f2(undefined);
 console.log("----------------------------------------------------------------------------------------------");
-// Both parameter can be optional.
+
 function f3(fname, lname) {
     console.log(fname);
     console.log(lname);
