79d0a7fe20baecb8e461256ec8e54c2057835cf4
IamVk009 <imvk009@gmail.com>
2021-08-31 19:25:21 +0530

    Object Added

--- 4] Object/TypeAlias.js
+++ 4] Object/TypeAlias.js
@@ -1,4 +1,4 @@
-// In order to create multiple objects with same type, we use type aliases.
+
 var emp1 = {
     id: 1,
     name: "Raj"
@@ -7,6 +7,6 @@ var emp2 = {
     id: 2,
     name: "Shyam"
 };
-// console.log(emp);
+
 console.log(emp1);
 console.log(emp2);
